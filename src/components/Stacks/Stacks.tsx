2d02c5abdc2bcd1c96dd100bb2541fbe2d059f40
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-03-20 09:50:54 -0300

    add new stack: materialUI

--- src/components/Stacks/Stacks.tsx
+++ src/components/Stacks/Stacks.tsx
@@ -69,6 +69,7 @@ const Stacks = () => {
             ></path>
           </svg>
         </li>
+        
         {/* TAILWINDCSS */}
         <li>
           <svg viewBox="0 0 128 128" width="78px" height="78px">
@@ -130,6 +131,25 @@ const Stacks = () => {
                 fill="#5a29e4"
               ></path>
             </g>
+          </svg>
+        </li>
+
+        {/* MATERIAL UI */}
+        <li>
+          <svg viewBox="0 0 128 128" width="78px" height="78px">
+            <path
+              fill="#1FA6CA"
+              d="M.2 68.6V13.4L48 41v18.4L16.1 41v36.8L.2 68.6z"
+            ></path>
+            <path
+              fill="#1C7FB6"
+              d="M48 41l47.9-27.6v55.3L64 87l-16-9.2 32-18.4V41L48 59.4V41z"
+            ></path>
+            <path fill="#1FA6CA" d="M48 77.8v18.4l32 18.4V96.2L48 77.8z"></path>
+            <path
+              fill="#1C7FB6"
+              d="M80 114.6L127.8 87V50.2l-16 9.2v18.4L80 96.2v18.4zM111.9 41V22.6l16-9.2v18.4l-16 9.2z"
+            ></path>
           </svg>
         </li>
 
@@ -279,7 +299,7 @@ const Stacks = () => {
             ></path>
           </svg>
         </li>
-        
+
         {/* GRAPHQL */}
         <li>
           <svg viewBox="0 0 128 128" width="78px" height="78px">
