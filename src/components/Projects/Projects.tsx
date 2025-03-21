e5ad568731fc0df7cf40c0016d2a7a062bb9e7d2
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-03-21 15:39:59 -0300

    add new project

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -7,30 +7,64 @@ const Projects = () => {
       <h1 style={{ color: "#f0f8ff", marginTop: "5rem" }}>Projetos</h1>
 
       <Carousel fade>
-        <Carousel.Item >
-          <ImgProject file="../../assets/projects/acerteONumero.png" />
-          <Carousel.Caption>
-            <h3>Acerte o número</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+        <Carousel.Item>
+          <a
+            href="https://game-acerte-o-numero.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ImgProject file="../../assets/projects/acerteONumero.png" />
+            <Carousel.Caption>
+              <h3>Acerte o número</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </a>
         </Carousel.Item>
 
         <Carousel.Item>
-          <ImgProject file="../../assets/projects/aluGames.png" />
-          <Carousel.Caption>
-            <h3>AluGames</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
+          <a
+            href="https://alu-games-seven.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ImgProject file="../../assets/projects/aluGames.png" />
+            <Carousel.Caption>
+              <h3>AluGames</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </a>
         </Carousel.Item>
 
         <Carousel.Item>
-          <ImgProject file="../../assets/projects/aluraSpa.png" />
-          <Carousel.Caption>
-            <h3>AluraSPA</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
+          <a
+            href="https://sass-nine-chi.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ImgProject file="../../assets/projects/aluraSpa.png" />
+            <Carousel.Caption>
+              <h3>AluraSPA</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </a>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <a
+            href="https://amigo-secreto-nine-lemon.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ImgProject file="../../assets/projects/amigoSecreto.png" />
+            <Carousel.Caption>
+              <h3>Amigo Secreto</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </a>
         </Carousel.Item>
       </Carousel>
     </>
